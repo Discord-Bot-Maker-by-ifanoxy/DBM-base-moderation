230f8043d31630279d1b6d531ce3f0dae79ea8fc
Ifanoxy <alwindoudou@gmail.com>
2024-06-19 11:09:14 +0200

    fix: add intents in config

--- dist/src/functions/hasBetterPermsThan.js
+++ dist/src/functions/hasBetterPermsThan.js
@@ -2,9 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.hasBetterPermsThan = void 0;
 function hasBetterPermsThan(mod, target) {
+    if (target.guild.ownerId === mod.id)
+        return true;
     if (target.guild.ownerId === target.id)
         return false;
-    if (target.permissions.has(mod.permissions))
+    if (target.permissions.has(mod.permissions.bitfield))
         return false;
     if (target.roles.highest.position >= mod.roles.highest.position)
         return false;
--- src/src/functions/hasBetterPermsThan.ts
+++ src/src/functions/hasBetterPermsThan.ts
@@ -2,8 +2,9 @@ import {GuildMember} from "discord.js";
 
 export function hasBetterPermsThan(mod: GuildMember, target: GuildMember): boolean
 {
+    if (target.guild.ownerId === mod.id)return true;
     if (target.guild.ownerId === target.id)return false;
-    if (target.permissions.has(mod.permissions))return false;
+    if (target.permissions.has(mod.permissions.bitfield))return false;
     if (target.roles.highest.position >= mod.roles.highest.position)return false;
     return true;
 }
